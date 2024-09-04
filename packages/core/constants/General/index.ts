0d18ac5c839a140ff8a5eab152d09ce712e8ae58
André Luiz Ceasar <andreceasar69@gmail.com>
2024-09-04 20:58:28 -0300

    chore: atualização de imagem e favicon

--- packages/core/constants/General/index.ts
+++ packages/core/constants/General/index.ts
@@ -11,7 +11,7 @@ export const defaultUrl = githubUrl;
 export * as favicon from './favicon.ico';
 
 // using bot app's header and resume's og:image
-export * as authorImage from './author.png';
+export * as authorImage from './author.jpg';
 
 // using blog og:image
 export * as defaultMetaBackground from './defaultMetaBackground.png';
